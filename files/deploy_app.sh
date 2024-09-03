e2bd52e2e07903483e820b7263e9ad16baa5d35a
ushassymon <ushassymon@yahoo.com>
2024-09-03 04:22:29 +0000

    first commit

---
+++ files/deploy_app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+
+# Script to deploy a very simple web application.
+# The web app has a customizable image and some text.
+
+cat << EOM > /var/www/html/index.html
+<html>
+  <head><title>Meow!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+
+  <!-- BEGIN -->
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><h2>Meow World!</h2></center>
+  Welcome to ${PREFIX}'s app. 
+
+  It is a wonderful world of cow's cout there'.
+
+  Welcome to the wilderness for AWS -> Hashicorp -> Palo -> and devops_1
+
+  <!-- END -->
+
+  </div>
+  </body>
+</html>
+EOM
+
+echo "Script complete."
